f61e287c2f34f6867b9a049892a51e8fb8bbbcd7
Sony Barrios <sony_barrios@usmp.pe>
2024-04-13 08:58:29 -0500

    primer commit con gitignore

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace grupo_rojo_repository.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
